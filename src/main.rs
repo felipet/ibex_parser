83f14b8d2008859361438facb1614347497ce1b9
Felipe Torres González <torresfelipex1@gmail.com>
2024-02-12 15:15:15 +0100

    Minimal main
    A minimal main function with a simple parsing of arguments.
    Changes:
    - Add clap as dependency of the crate.
    - Add code to handle argurments to main function.
    - Add some simple logic to filter out empty files.

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,58 @@
 use ibex_parser::discover;
 use ibex_parser::parser_ibex::IbexParser;
 use std::path::Path;
+use clap::Parser;
+
+// The minium size of a text file that might contain stock data. Files with less than this size are omitted.
+const MIN_BYTES_X_FILE: u64 = 560;
+
+#[derive(Parser, Debug)]
+#[command(name = "IbexParser")]
+#[command(version = "0.1.0")]
+#[command(about = "Parser for Ibex35 stock data", long_about = r#"
+Ibex35 Data Parsing Tool: This tool parses stock prices and other data as is offered by BME's web.
+Data is parsed from raw text files and output in CSV format for a later import into some analysis tool
+or graph tool.
+
+Raw text files shall keep the same data organization as BME's web does. For example, select all the content
+of the page and paste it into a text file. That file is ready to be used by this parser.
+"#)]
+struct Args {
+    /// Directory to search for text data files.
+    path: String,
+    /// Company to filter the results.
+    filter: Option<String>,
+}
 
 fn main() {
-    let dir = "./tests/data";
-    let path = Path::new(dir);
+    let args = Args::parse();
+    let filter: Vec<String> = if let Some(filter) = args.filter.as_deref() {
+        vec![String::from(filter)]
+    } else {
+        Vec::new()
+    };
+
+    let path = Path::new(&args.path);
     let files = discover(path, None, None);
     let parser = IbexParser::new();
-    let mut data: Vec<String>;
 
     for file in files {
-        println!("Parsing file: {:?}", file);
-        let filepath = format!("{}/{}",dir,file.as_str());
-        data = parser.parse_file(Path::new(&filepath)).unwrap();
+        let file_string = format!("{}/{}",&args.path,file.as_str());
+        let path = Path::new(&file_string);
 
-        for line in data {
-            println!("{:?}", line);
+        // Avoid passing empty files to the parser.
+        if path.metadata().unwrap().len() < MIN_BYTES_X_FILE {
+            continue;
+        }
+        let data = parser.filter_file(path , &filter);
+
+        match data {
+            Some(x) => {
+                for line in x {
+                    println!("{}", line);
+                }
+            },
+            None => println!("File {file} doesn't contain valid data.")
         }
     }
-
 }
